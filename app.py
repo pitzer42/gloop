fa526e7739afd6aad93166607ec0fec506d6b97a
Arthur Pitzer <arthurpitzer@id.uff.br>
2021-01-24 02:17:05 +0000

    templates

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+import jinja2
 from aiohttp import web
 
+TEAMPLATES_PATH = 'templates'
+TEAMPLATE_EXTENTION = '.html'
 
 db = []
+
+
+template_loader = jinja2.FileSystemLoader(searchpath=TEAMPLATES_PATH)
+template_env = jinja2.Environment(loader=template_loader)
+
+
+def template_for(view):
+    template_name = view.__name__ + TEAMPLATE_EXTENTION
+    return template_env.get_template(template_name)
+
+
+def html_response(html):
+    return web.Response(text=html, content_type='text/html')
 
 
 def redirect(request, view):
@@ -11,34 +27,54 @@ def redirect(request, view):
 
 
 async def index(request):
-    return web.Response(text=str(db))
-
-
-async def create_or_details(request):
-    _id = request.match_info['_id']
-    if _id in db:
-        return await details(request)
-    return await create(request)
+    template = template_for(index)
+    create_view_url = request.app.router[create.__name__].canonical
+    rendered = template.render(items=db, create_view_url=create_view_url)
+    return html_response(rendered)
 
 
 async def create(request):
-    _id = request.match_info['_id']
-    db.append(_id)
-    return redirect(request, index)
+
+    if request.method == 'GET':
+        template = template_for(create)
+        rendered = template.render()
+        return html_response(rendered)
+    
+    if request.method == 'POST':
+        data = await request.post()
+        _id = data['_id']
+        _description = data['_description']
+        _size = data['_size']
+        db.append((_id, _description, _size))
+        return redirect(request, index)
 
 
 async def details(request):
     _id = request.match_info['_id']
-    return web.Response(text=str(_id))
+    room = None
+    for item in db:
+        if item[0] == _id:
+            room = item
+            break
+    if room:
+        return web.Response(text=str(_id))
+    raise web.HTTPNotFound()
 
 
 def create_route_get(path, handler):
     return web.get(path, handler, name=handler.__name__)
 
+def create_route_post(path, handler):
+    return web.post(path, handler, name=handler.__name__)
+
 
 app = web.Application()
+
 app.add_routes([
     create_route_get('/', index),
-    create_route_get('/{_id}', create_or_details),
+    create_route_get('/create', create),
+    create_route_post('/create', create),
+    create_route_get('/{_id}', details),
 ])
+
 web.run_app(app)
